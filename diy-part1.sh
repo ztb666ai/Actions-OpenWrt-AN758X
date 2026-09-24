cc78b742b88fba4bc5cad2b35d50e2417111965b
ztb666ai <ztb666ai@gmail.com>
2026-09-24 20:44:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -43,9 +43,9 @@ mkdir -p package/custom
 #git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
 #git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
 
-git clone --depth=1 https://github.com/stackia/rtp2httpd.git package/custom/rtp2httpd
+#git clone --depth=1 https://github.com/stackia/rtp2httpd.git package/custom/rtp2httpd
 
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
 #git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/custom/OpenAppFilter
@@ -59,13 +59,9 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/cu
 # 主题
 # 必须先删掉 feeds 自带版本，否则会与 package/custom 这份同名冲突，
 # 导致 configs 里 CONFIG_PACKAGE_luci-theme-argon=y 编译时用哪一份不确定
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
-git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/luci-theme-argon.git package/custom/luci-theme-argon
-git clone --depth=1 https://github.com/sbwml/luci-app-argon-config.git package/custom/luci-app-argon-config
-
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git package/custom/luci-theme-aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git package/custom/luci-app-aurora-config
-
-git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/custom/luci-theme-kucat
-git clone --depth=1 https://github.com/sirpdboy/luci-app-kucat-config.git package/custom/luci-app-kucat-config
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/luci-theme-argon.git package/custom/luci-theme-argon
+#git clone --depth=1 https://github.com/sbwml/luci-app-argon-config.git package/custom/luci-app-argon-config
+# change the default theme:
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g; s/default Bootstrap theme/Argon theme/g' feeds/luci/collections/luci-light/Makefile
